4cecead1ec8f9e0a074bf55e3c30e14a28364dec
Bright Ihegawam <bright06iheagwam@gmail.com>
2025-11-18 06:43:56 +0100

    created wallet schema

---
+++ src/models/WalletSchema.ts
@@ -0,0 +1,19 @@
+import pool from "../utils/database/db";
+
+const createWalletTable = async ()=>{
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS wallets(
+            id SERIAL PRIMARY KEY,
+            user_id UNIQUE NOT NULL REFERENCES user(id) ON DELETE CASCADE,
+            amount NUMERIC(12,2) DEFAULT 0 CHECK (amount>=0),
+            currency VARCHAR(6) DEFAULT 'NGN',
+            created_at TIMESTAMP DEFAULT NOT NULL CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT NOT NULL CURRENT_TIMESTAMP
+            )
+            `)
+    } catch (error) {
+        
+    }
+}
+
